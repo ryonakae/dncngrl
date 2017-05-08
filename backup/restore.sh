54336498aeec9b4a83fa3ecc92ce7a1edca38411
Ryo Nakae <ryo.n.1129@gmail.com>
2017-05-09 00:08:56 +0900

    ディレクトリ構造変更

---
+++ backup/restore.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# restore wp-data
+docker run --rm -it --volumes-from brdr-data --volume $(pwd)/backup/data:/backup busybox tar xvf /backup/wp-data.tar
+
+# restore db-data
+docker run --rm -it --volumes-from brdr-data --volume $(pwd)/backup/data:/backup busybox tar xvf /backup/db-data.tar
+
+# restart mysql service
+docker restart dncngrl-mysql
+
+# delete backup files
+# rm -rf $(pwd)/backup/data/db.sql
+# rm -rf $(pwd)/backup/data/wp-data.tar
