c75fc066174fd0533f9a014b6deebe72125d76c2
Ryo Nakae <ryo.n.1129@gmail.com>
2017-05-17 18:40:09 +0900

    add wp-cli container

---
+++ script/restore.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# restore wp-content/uploads
+docker-compose run --rm data tar xvf /backup/wp-content-uploads.tar
+
+# restore db by wp-cli
+docker-compose run --rm wpcli db import /backup/db.sql
+
+# restart
+docker-compose restart
+
+# delete backup files
+# rm -rf $(pwd)/backup/db.sql
+# rm -rf $(pwd)/backup/wp-content-uploads.tar
