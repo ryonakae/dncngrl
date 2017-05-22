48db5307edc1dbcd2b84983de01cda69acd7a115
Ryo Nakae <ryo.n.1129@gmail.com>
2017-05-22 16:58:28 +0900

    VuexのHot Module Replacement対応

--- theme/dev/assets/scripts/store/index.js
+++ theme/dev/assets/scripts/store/index.js
@@ -1,22 +1,42 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-Vue.use(Vuex);
-
 import mutations from './mutations';
 import actions from './actions';
 
-export default new Vuex.Store({
+Vue.use(Vuex);
+
+const state = {
+  pageTitle: '',
+  siteTitle: document.title,
+  siteUrl: process.env.NODE_ENV === 'production' ? location.protocol + '//' + location.host : 'http://dncngrl.dev',
+  allPostData: [],
+  currentPostData: {}
+};
+
+const store = new Vuex.Store({
   // development時のみ厳格モード有効
   strict: process.env.NODE_ENV !== 'production',
 
-  state: {
-    pageTitle: '',
-    siteTitle: document.title,
-    siteUrl: process.env.NODE_ENV === 'production' ? location.protocol + '//' + location.host : 'http://dncngrl.dev',
-    allPostData: [],
-    currentPostData: {}
-  },
-
+  state,
   mutations,
   actions
 });
+
+// Hot Module Replacementに対応させる
+if (module.hot) {
+  module.hot.accept([
+    './mutations',
+    './actions'
+  ], ()=>{
+    // import modules
+    const newMutations = require('./mutations').default;
+    const newActions = require('./actions').default;
+
+    store.hotUpdate({
+      mutations: newMutations,
+      actions: newActions
+    });
+  });
+}
+
+export default store;
