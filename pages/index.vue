e901048d9d0a3c9ce84cbe9ddb34de01cc930cf5
Ryo Nakae <ryo.n.1129@gmail.com>
2021-12-13 12:00:11 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,14 @@
   <h1 class="title">Hello World!</h1>
 </template>
 
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+})
+</script>
+
 <style lang="scss" scoped>
 .title {
   color: blue;
